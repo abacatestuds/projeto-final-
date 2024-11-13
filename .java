2376ff6eaa6b1cead6c113a7fc7c52eafd120a16
abacatestuds <Abac4testuds@gmail.com>
2024-11-13 19:09:09 -0300

    termina essa md

---
+++ .java
@@ -0,0 +1,16 @@
+function searchMovies() {
+    let input = document.getElementById("searchinput").value.toLowerCase();
+    let movieList = document.getElementById("movieList");
+    let movies = movieList.getElementsByClassName("movie-item");
+
+    // Loop pelos filmes e séries
+    for (let i = 0; i < movies.length; i++) {
+        let title = movies[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+
+        if (title.includes(input)) {
+            movies[i].style.display = ""; // Exibe o item que corresponde à busca
+        } else {
+            movies[i].style.display = "none"; // Oculta o item que não corresponde
+        }
+    }
+}
